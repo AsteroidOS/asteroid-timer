3ec8f18c776c341777388abe47cf925c9e2c2f56
Andrea Scarpino <me@andreascarpino.it>
2017-04-01 11:11:07 +0000

    Added translation using Weblate (Italian)

---
+++ i18n/asteroid-timer.it.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="asteroid-timer.desktop.h" line="5"/>
+        <source>Timer</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
