43868e4c8ecc51336fab566e910cd8c7dbd0b5c0
sam bowlby <sam@chven.us>
2017-11-09 06:58:01 +0000

    Translated using Weblate (Georgian)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Timer
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/timer/ka/

--- i18n/asteroid-timer.ka.ts
+++ i18n/asteroid-timer.ka.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="asteroid-timer.desktop.h" line="5"/>
         <source>Timer</source>
-        <translation></translation>
+        <translation>ტაიმერი</translation>
     </message>
 </context>
 </TS>
