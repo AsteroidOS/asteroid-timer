a3cd6e6a3baf5a640c3aaeebce68bb890992ac4a
Florent Revest <revestflo@gmail.com>
2017-11-25 00:06:25 +0100

    Add a default english translation

---
+++ i18n/asteroid-timer.en_GB.ts
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="fr">
+<context>
+    <name></name>
+    <message id="id-app-launcher-name">
+        <location filename="asteroid-timer.desktop.h" line="6"/>
+        <source>Timer</source>
+        <translation>Timer</translation>
+    </message>
+</context>
+</TS>
