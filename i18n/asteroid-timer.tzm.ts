9bb9679c884fbcebbe52d373a554522223790c49
Hakim Oubouali <hakim.oubouali.skr@gmail.com>
2020-10-14 12:02:32 +0000

    Translated using Weblate (Central Atlas Tamazight)
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Timer
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/timer/tzm/

--- i18n/asteroid-timer.tzm.ts
+++ i18n/asteroid-timer.tzm.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-timer.desktop.h" line="6"/>
         <source>Timer</source>
-        <translation type="unfinished"></translation>
+        <translation>Amesiḍen n wakud</translation>
     </message>
 </context>
 </TS>
