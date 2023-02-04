3371a2cef5059ff040af4aaaedc6c6fa5301ce02
ⵣⵓⵀⵉⵔ ⴰⵎⴰⵣⵉⵖ ZOUHIR DEHBI <zouhirdehbi56@gmail.com>
2023-02-04 21:36:35 +0000

    Translated using Weblate (Tamazight (Standard Moroccan))
    Currently translated at 100.0% (1 of 1 strings)
    
    Translation: AsteroidOS/Timer
    Translate-URL: https://hosted.weblate.org/projects/asteroidos/timer/zgh/

--- i18n/asteroid-timer.zgh.ts
+++ i18n/asteroid-timer.zgh.ts
@@ -6,7 +6,7 @@
     <message id="id-app-launcher-name">
         <location filename="asteroid-timer.desktop.h" line="6"/>
         <source>Timer</source>
-        <translation type="unfinished"></translation>
+        <translation>ⴰⵎⵙⵉⴹⵏ</translation>
     </message>
 </context>
 </TS>
